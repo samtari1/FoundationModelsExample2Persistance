192b80f6e65539969448a416486b5bf63ef63ec9
samtari1 <samtariyang@gmail.com>
2026-03-05 09:42:18 -0500

    Initial Commit

---
+++ FoundationModelsExample2Persistance/FoundationModelsExample2PersistanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationModelsExample2PersistanceApp.swift
+//  FoundationModelsExample2Persistance
+//
+//  Created by Quanpeng Yang on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct FoundationModelsExample2PersistanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FoundationModelsExample2PersistanceTests/FoundationModelsExample2PersistanceTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationModelsExample2PersistanceTests.swift
+//  FoundationModelsExample2PersistanceTests
+//
+//  Created by Quanpeng Yang on 3/5/26.
+//
+
+import Testing
+@testable import FoundationModelsExample2Persistance
+
+struct FoundationModelsExample2PersistanceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
